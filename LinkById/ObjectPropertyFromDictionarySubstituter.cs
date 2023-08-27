f58690cfeaf1325b1bb262d9193aae2e090bec6b
Vlad Perepelytsya <vldp2000@gmail.com>
2023-08-27 15:19:26 +1000

    add new helper with substitute methods
    add new helper with substitute methods

---
+++ LinkById/ObjectPropertyFromDictionarySubstituter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+
+namespace JWTTestWebApi.Services.ServiceHelper
+{
+    public class ObjectPropertyFromDictionarySubstituter
+    {
+        private static ObjectPropertyFromDictionarySubstituter _instrance;
+
+        private ObjectPropertyFromDictionarySubstituter()
+        {
+        }
+
+        public static ObjectPropertyFromDictionarySubstituter GetInstanse()
+        {
+            if (_instrance == null)
+                _instrance = new ObjectPropertyFromDictionarySubstituter();
+            return _instrance;
+        }
+
+        public static Int32? SubstituteIntPropertyValueUsingProvidedDictionary(Object obj, string propertyName, Dictionary<int, int> dict)
+        {
+            int? result = null;
+            var property = obj.GetType().GetProperty(propertyName);
+            if (property != null && property.PropertyType == typeof(Int32) && dict != null)
+            {
+                var propertyValue = property.GetValue(obj, null);
+
+                if (propertyValue is int value && propertyValue != null)
+                {
+                    if (dict.TryGetValue(value, out var newValue))
+                        result = newValue;
+                    property.SetValue(obj, newValue, null);
+                }
+            }
+            return result;
+        }
+
+        public static string? SubstituteStringPropertyValueUsingProvidedDictionary(Object obj, string propertyName, Dictionary<string, string> dict)
+        {
+            string? result = null;
+            var property = obj.GetType().GetProperty(propertyName);
+            if (property != null && property.PropertyType == typeof(string) && dict != null)
+            {
+                var propertyValue = property.GetValue(obj, null);
+
+                if (propertyValue is string value && propertyValue != null)
+                {
+                    if (dict.TryGetValue(value, out var newValue))
+                        result = newValue;
+                    property.SetValue(obj, newValue, null);
+                }
+            }
+            return result;
+        }
+
+    }
+}
--- LinkById/Program.cs
+++ LinkById/Program.cs
@@ -1,30 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using JWTTestWebApi.Services.ServiceHelper;
 using LinkById;
 
 Console.WriteLine("Linking dummy Ids to actual Ids.");
 
 List<Item1> item1List = new List<Item1>(){new Item1(-1), new Item1(-2), new Item1(-3)};
-List<Item2> item2List = new List<Item2>(){new Item2(-5), new Item2(-6), new Item2(-7)};
+List<Item2> item2List = new List<Item2>(){new Item2(-5, "Code-5"), new Item2(-6, "Code-6"), new Item2(-7,"Code-7") };
 
-var substituteRefJobForItem1 = new Dictionary<int, int>() { { -1, 111 }, { -2, 22 }, { -3, 33 } };
-var substituteIdForItem2 = new Dictionary<int, int>() { { -5, 55 }, { -6, 66 }, { -7, 77 } };
-var substituteIdForItem2 = new Dictionary<int, int>() { { -5, 55 }, { -6, 66 }, { -7, 77 } };
+Dictionary<int, int> substituteRefJobForItem1 = new Dictionary<int, int>();
+substituteRefJobForItem1.Add(-1, 111);
+substituteRefJobForItem1.Add(-2, 22);
+substituteRefJobForItem1.Add(-3, 33);
 
-var substituteForItem2 = new Dictionary<int, int>() { { -5, 55 }, { -6, 66 }, { -7, 77 } };
+Dictionary<int, int> substituteIdForItem2 = new Dictionary<int, int>() { { -5, 55 }, { -6, 66 }, { -7, 77 } };
+
 
 Item1.AddLinkedIdList(typeof(Item1), substituteRefJobForItem1);
 Item2.AddLinkedIdList(typeof(Item2), substituteIdForItem2);
 
-foreach (var item in item1List)
-{
-    Console.WriteLine($" -->ILinkedById. Collection {nameof(item1List)}. RefJob = {item.RefJob}. New value = {item.LinkedValue} ");
-    Console.WriteLine($" >> ISubstitute. Collection {nameof(item1List)}. RefJob = {item.RefJob}. New value = {item.FindSubstituteValue("RefJob", substituteRefJobForItem1)} ");
-}
+if (substituteRefJobForItem1 != null)
+    foreach (var item in item1List) 
+    {
+        Console.WriteLine($" -->ILinkedById. Collection {nameof(item1List)}. RefJob = {item.RefJob}. New value = {item.LinkedValue} ");
+        Console.WriteLine($" >> ISubstitute. Collection {nameof(item1List)}. RefJob = {item.RefJob}. New value = {item.FindSubstituteValue("RefJob", substituteRefJobForItem1)} ");
+        int? refjob = ObjectPropertyFromDictionarySubstituter.SubstituteIntPropertyValueUsingProvidedDictionary(obj: item, propertyName: "RefJob", dict: substituteRefJobForItem1);
+        Console.WriteLine($" - Substituter. Collection {nameof(item1List)}. RefJob = {item.RefJob}.  Substitute value-> {refjob}");
+    }
 
 
 foreach (var item in item2List)
 {
     Console.WriteLine($" -->ILinkedById. Collection {nameof(item2List)}. Id = {item.Id}. New value= {item.LinkedValue} ");
     Console.WriteLine($" >> ISubstitute. Collection {nameof(item1List)}. Id = {item.Id}. New value = {item.FindSubstituteValue("Id", substituteIdForItem2)} ");
+    int? id = ObjectPropertyFromDictionarySubstituter.SubstituteIntPropertyValueUsingProvidedDictionary(obj: item, propertyName: "Id", dict: substituteIdForItem2);
+    Console.WriteLine($" - Substituter. {nameof(item1List)}. Id = {item.Id}.  Substitute value-> {id}");
 }
 Item1.CleanLinkedValues();
